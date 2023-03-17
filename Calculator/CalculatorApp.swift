f0bef13b797ca49d59118ae26e1568384777c6f0
Komal Gilani <komalsyeda29@gmail.com>
2023-03-17 14:16:56 +0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Komal Gilani on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
